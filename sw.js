00eb26742a1aa150170620c426f79592a608a528
rainbowetdash <rainbowetdash@gmail.com>
2026-07-24 03:29:59 -0500

    fix: theme schedule group delete button

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const cache = 'memory-everyday-v37';
-const files = ['/', '/index.html', '/styles.css?v=37', '/calendar-month.css?v=37', '/app.js?v=37', '/manifest.webmanifest', '/icon.svg'];
+const cache = 'memory-everyday-v38';
+const files = ['/', '/index.html', '/styles.css?v=38', '/calendar-month.css?v=38', '/app.js?v=38', '/manifest.webmanifest', '/icon.svg'];
 
 self.addEventListener('install', (event) => event.waitUntil(
   caches.open(cache).then((storage) => storage.addAll(files)).then(() => self.skipWaiting())
