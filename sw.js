96c3e764aa6cd2f2568f8750b0062182dc11c876
rainbowetdash <rainbowetdash@gmail.com>
2026-07-24 01:48:54 -0500

    feat: add schedule groups sidebar

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const cache = 'memory-everyday-v33';
-const files = ['/', '/index.html', '/styles.css?v=33', '/calendar-month.css?v=33', '/app.js?v=33', '/manifest.webmanifest', '/icon.svg'];
+const cache = 'memory-everyday-v34';
+const files = ['/', '/index.html', '/styles.css?v=34', '/calendar-month.css?v=34', '/app.js?v=34', '/manifest.webmanifest', '/icon.svg'];
 
 self.addEventListener('install', (event) => event.waitUntil(
   caches.open(cache).then((storage) => storage.addAll(files)).then(() => self.skipWaiting())
